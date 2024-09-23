cff70bb2cc46021b9089d10c4e31a12400aa1623
SarkaKv <145465445+SarkaKv@users.noreply.github.com>
2024-09-23 15:23:42 +0200

    Add files via upload

---
+++ client.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skvackov <skvackov@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 12:55:50 by skvackov          #+#    #+#             */
+/*   Updated: 2024/09/23 15:14:59 by skvackov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int plzencrypt(char* message)
+{
+	int messagetosend;
+
+
+}
+
+int main(char** argv, int argc)
+{
+	int message;
+	int PID;
+	if(argc != 3)
+	{
+	ft_printf("Error");
+	return(1);
+	}
+	else
+	{
+		message = plzencrypt(argv[1]);
+
+
+	}
+}
