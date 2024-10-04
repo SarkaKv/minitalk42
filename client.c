3d9a2bdd5a8bfc82675e5f5574f3e70ab5b11c1f
SarkaKv <145465445+SarkaKv@users.noreply.github.com>
2024-10-04 15:05:48 +0200

    Add files via upload

--- client.c
+++ client.c
@@ -6,30 +6,99 @@
 /*   By: skvackov <skvackov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 12:55:50 by skvackov          #+#    #+#             */
-/*   Updated: 2024/09/23 15:14:59 by skvackov         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:56:10 by skvackov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int plzencrypt(char* message)
+#include <signal.h>
+#include <unistd.h>
+
+static char	*plzencrypt(char message)
 {
-	int messagetosend;
+	static char	messagetosend[9];
+	int			numberofbit;
+	int			chartobit;
 
-
+	numberofbit = 7;
+	chartobit = (int)message;
+	while (numberofbit >= 0)
+	{
+		if ((chartobit >> numberofbit) & 1)
+			messagetosend[7 - numberofbit] = '1';
+		else
+			messagetosend[7 - numberofbit] = '0';
+		numberofbit--;
+	}
+	messagetosend[8] = '\0';
+	return (messagetosend);
 }
 
-int main(char** argv, int argc)
+static void	plzsend(int pid, char *message)
 {
-	int message;
-	int PID;
-	if(argc != 3)
+	int	count;
+
+	count = 0;
+	while (message[count] != '\0')
 	{
-	ft_printf("Error");
-	return(1);
+		if (message[count] == '1')
+		{
+			if (kill(pid, SIGUSR1) == -1)
+			{
+				ft_printf("ERROR sending sigusr1");
+				exit(1);
+			}
+		}
+		else if (message[count] == '0')
+		{
+			if (kill(pid, SIGUSR2) == -1)
+			{
+				ft_printf("ERROR sending sigusr2");
+				exit(1);
+			}
+		}
+		count++;
+		usleep(1000);
+	}
+}
+
+static void	killsignals(int pid)
+{
+	int	oki;
+
+	oki = 0;
+	while (oki < 8)
+	{
+		if (kill(pid, SIGUSR2) == -1)
+		{
+			ft_printf("ERROR sending sigusr2");
+			exit(1);
+		}
+		oki++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int		pid;
+	char	*themessage;
+	int		count;
+
+	pid = ft_atoi(argv[1]);
+	count = 0;
+	if (argc != 3 || pid <= 0)
+	{
+		ft_printf("Error wrong PID or wrong number of arguments");
+		return (1);
 	}
 	else
 	{
-		message = plzencrypt(argv[1]);
-
-
+		while (argv[2][count] != '\0')
+		{
+			themessage = plzencrypt(argv[2][count]);
+			plzsend(pid, themessage);
+			count++;
+		}
+		killsignals(pid);
 	}
+	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,74 @@
 /*   By: skvackov <skvackov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 12:55:53 by skvackov          #+#    #+#             */
-/*   Updated: 2024/09/23 12:55:54 by skvackov         ###   ########.fr       */
+/*   Updated: 2024/10/04 14:59:20 by skvackov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+char		g_getbit[9];
+
+static void	sigusr_handler(int sig)
+{
+	static int	count = 0;
+
+	if (sig == SIGUSR1 && count < 8)
+	{
+		g_getbit[count] = '1';
+		count++;
+		usleep(500);
+	}
+	else if (sig == SIGUSR2 && count < 8)
+	{
+		g_getbit[count] = '0';
+		count++;
+		usleep(500);
+	}
+	if (count == 8)
+	{
+		g_getbit[count] = '\0';
+		if (ft_strcmp(g_getbit, "00000000") == 0)
+		{
+			exit(0);
+		}
+		count = 0;
+		translprint(g_getbit);
+	}
+}
+
+static void	translprint(char *totranslate)
+{
+	int		asciifirst;
+	int		counter;
+	char	chartoprint;
+
+	asciifirst = 0;
+	counter = 0;
+	while (counter < 8)
+	{
+		if (totranslate[counter] == '1')
+		{
+			asciifirst += (1 << (7 - counter));
+		}
+		counter++;
+	}
+	chartoprint = (char)asciifirst;
+	printf("%c", chartoprint);
+}
+
+int	main(void)
+{
+	int	pid;
+
+	pid = getpid();
+	ft_printf("process ID: %d\n", pid);
+	signal(SIGUSR1, sigusr_handler);
+	signal(SIGUSR2, sigusr_handler);
+	while (1)
+	{
+		pause();
+	}
+}
